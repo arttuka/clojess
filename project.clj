f5b78192b44ef391d80bf5cacd0bd622fd6e87e7
Arttu Kaipiainen <arttu.kaipiainen@tut.fi>
2014-01-15 21:09:02 +0200

    Fixed jackcess dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clojess "0.2"
+(defproject clojess "0.3"
   :description "An MS Access libary for Clojure, wrapping Jackcess"
   :url "https://github.com/arttuka/clojess"
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
+  :license {:name "LGPL License"
+            :url "http://opensource.org/licenses/LGPL-3.0"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.healthmarketscience/jackcess "2.0.2"]
+                 [com.healthmarketscience.jackcess/jackcess "2.0.2"]
                  [commons-logging/commons-logging "1.1.3"]
                  [commons-lang/commons-lang "2.6"]
                  [org.apache.poi/poi "3.9"]
